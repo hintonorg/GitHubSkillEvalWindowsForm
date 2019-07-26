7e3cdafed7df582a84b923fcef6c6191c0f645d5
Dwayne Hinton <dwayneahinton@gmail.com>
2019-07-25 22:40:59 -0600

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace GitHubSkillEval
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScanForLicenses = new System.Windows.Forms.Button();
+            this.btnCreateLicenses = new System.Windows.Forms.Button();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnScanForLicenses
+            // 
+            this.btnScanForLicenses.Location = new System.Drawing.Point(58, 272);
+            this.btnScanForLicenses.Name = "btnScanForLicenses";
+            this.btnScanForLicenses.Size = new System.Drawing.Size(140, 23);
+            this.btnScanForLicenses.TabIndex = 0;
+            this.btnScanForLicenses.Text = "Only Scan For Licenses";
+            this.btnScanForLicenses.UseVisualStyleBackColor = true;
+            this.btnScanForLicenses.Click += new System.EventHandler(this.BtnScanForLicenses_Click);
+            // 
+            // btnCreateLicenses
+            // 
+            this.btnCreateLicenses.Location = new System.Drawing.Point(274, 272);
+            this.btnCreateLicenses.Name = "btnCreateLicenses";
+            this.btnCreateLicenses.Size = new System.Drawing.Size(140, 23);
+            this.btnCreateLicenses.TabIndex = 1;
+            this.btnCreateLicenses.Text = "Scan and Create Licenses";
+            this.btnCreateLicenses.UseVisualStyleBackColor = true;
+            this.btnCreateLicenses.Click += new System.EventHandler(this.BtnCreateLicenses_Click);
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(58, 26);
+            this.txtList.Multiline = true;
+            this.txtList.Name = "txtList";
+            this.txtList.ReadOnly = true;
+            this.txtList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtList.Size = new System.Drawing.Size(356, 230);
+            this.txtList.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 317);
+            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.btnCreateLicenses);
+            this.Controls.Add(this.btnScanForLicenses);
+            this.Name = "MainForm";
+            this.Text = "GitHub Skill Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScanForLicenses;
+        private System.Windows.Forms.Button btnCreateLicenses;
+        private System.Windows.Forms.TextBox txtList;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,129 @@
+﻿using Octokit;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitHubSkillEval
+{
+    public partial class MainForm : Form
+    {
+        private static GitHubClient client;
+        private static string MyOrganization;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            string GetHubIdentity;
+            Credentials credentials;
+
+            GetHubIdentity = "GitHubSkillEval";
+            MyOrganization = "hintonorg";
+
+            try
+            {
+                client = new GitHubClient(new ProductHeaderValue(GetHubIdentity));
+
+                //credentials = new Credentials("133e3554909b83a8a81364661fe822e60d8ff466");    //My personal access token.  Does not work if I update GitHub with a new Id.
+                credentials = new Credentials("dwaynehinton", "Mygithubpwd1");                  //Login name and password
+                client.Credentials = credentials;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(this, e.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private async Task ScanForLicenses()
+        {
+            int iCnt = 0;
+
+            txtList.Text = "";
+
+            try
+            {
+                //Get all the repositories in the Organization.
+                var Repositories = await client.Repository.GetAllForOrg(MyOrganization);
+                foreach (var repo in Repositories)
+                {
+                    if (repo.License == null)
+                    {
+                        txtList.Text = txtList.Text + "*No License for the \"" + repo.Name.ToString() + "\" Repository\n\r\n\r";
+                    }
+                    else
+                    {
+                        txtList.Text = txtList.Text + repo.License.Name + " for the \"" + repo.Name.ToString() + "\" Repository\n\r\n\r";
+                    }
+
+                    iCnt++;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(this, e.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private async Task CreateLicenses()
+        {
+            string LicenseBody;
+            int iCnt = 0;
+
+            txtList.Text = "";
+
+            //Get a MIT License
+            var mitlicense = await client.Miscellaneous.GetLicense("mit");
+
+            try
+            {
+                //Get all the repositories in the Organization.
+                var Repositories = await client.Repository.GetAllForOrg(MyOrganization);
+                foreach (var repo in Repositories)
+                {
+                    if (repo.License == null)
+                    {
+                        LicenseBody = mitlicense.Body;
+
+                        //Change the [year], in the license body, to the current year. 
+                        LicenseBody = LicenseBody.Replace("[year]", DateTime.Now.Year.ToString());
+
+                        //Change the [fullname], in the license body, to the name of the repo.
+                        LicenseBody = LicenseBody.Replace("[fullname]", repo.Name);
+
+                        //Create a commit
+                        var createChangeSet = await client.Repository.Content.CreateFile(
+                                MyOrganization,
+                                repo.Name,
+                                "LICENSE",
+                                new CreateFileRequest("File creation",
+                                                      LicenseBody,
+                                                      "master"));
+
+
+                        txtList.Text = txtList.Text + "A \"MIT License\" was created for the \"" + repo.Name.ToString() + "\" Repository\n\r\n\r";
+                    }
+                    else
+                    {
+                        txtList.Text = txtList.Text + "Already had a \"MIT License\" for the \"" + repo.Name.ToString() + "\" Repository\n\r\n\r";
+                    }
+
+                    iCnt++;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(this, e.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnScanForLicenses_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () => { await ScanForLicenses(); });
+        }
+
+        private void BtnCreateLicenses_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () => { await CreateLicenses(); });
+        }
+    }
+}
